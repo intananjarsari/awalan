fce3f782dc6d7fc75dba41a20e9af2fdfbfc7d17
intananjarsari <70148082+intananjarsari@users.noreply.github.com>
2020-08-24 21:21:14 +0700

    Add files via upload

---
+++ Belajar1.php
@@ -0,0 +1,17 @@
+<?php
+class leptop {
+    var $pemilik;
+    var $merk;
+    var $ukuran_layar;
+function hidupkan_leptop () { 
+    return "Hidupkan leptop";
+    echo "<br>";
+}
+function matikan_leptop () {
+    return "Matikan leptop";
+    echo "<br>";
+}
+  }
+  $leptop_intan = new leptop ();
+  $leptop_anjar = new leptop ();
+  ?>
